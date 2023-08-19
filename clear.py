764fa20fac5b5edfff338db33ba67ae610752bbe
Sergei Kirikeza <serj280294@yandex.ru>
2023-08-19 14:59:36 +0300

    Finding files for clean

--- clear.py
+++ clear.py
@@ -1,5 +1,7 @@
 import os
 
+FILES_FOR_CLEAN_EXTENTIONS = ('.c', '.h')
+
 TEMP_FILE_PREFIX = "temp_"
 LINE_FOR_REMOVE_TEMPLATE = "/* USER CODE "
 
@@ -23,10 +25,26 @@ def clear_file(filename):
 
     return lines_counter
 
+def get_files_for_clean(generic_path):
+    """Returns file paths to clean up"""
+    paths = []
+    for root, dirs, files in os.walk(generic_path):
+        for file in files:
+            extension = os.path.splitext(file)[1]
+            if extension in FILES_FOR_CLEAN_EXTENTIONS:
+                paths.append(os.path.join(root, file))
+
+    return paths
+
 def main():
     """Finding and cleaning project files"""
 
-    print(f'Current directory path: {os.path.dirname(__file__)}')
+    generic_path = os.path.dirname(__file__)
+    print(f'Current directory path: {generic_path}')
+
+    files = get_files_for_clean(generic_path)
+    for path in files:
+        print(path)
 
     filename = 'some.c'
 
