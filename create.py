0055c799692ae52e5ed4f18f3515733d98922bbe
Sergei Kirikeza <serj280294@yandex.ru>
2023-08-27 15:10:55 +0300

    Configuration file

--- create.py
+++ create.py
@@ -1,19 +1,19 @@
+import json
 from jinja2 import Environment, FileSystemLoader
 
 TEMPLATES_DIR = "templates/"
 TEMPLATE_NAME = "base.txt"
-
-separators = ['HELLO', 'WORLD', 'HOW', 'ARE', 'YOU']
+INFO_FILE_NAME = "config.json"
 
 env = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
 template = env.get_template(TEMPLATE_NAME)
 
-def generate_file(filename):
+def generate_file(filename, info):
     context = {
         'filename': filename,
-        'author': 'Sergei Kirikeza',
-        'description': 'This is short description of module',
-        'separators': separators
+        'author': info.get('author_name') or "",
+        'description': info.get('description_template') or "",
+        'separators': info.get('separators') or []
     }
 
     generated = template.render(**context)
@@ -23,9 +23,13 @@ def generate_file(filename):
 def main():
     files = ['test.c', 'test.h']
 
+    # TODO: Information file verification function
+    with open(INFO_FILE_NAME, 'r', encoding='utf-8') as f:
+        info = json.load(f)
+
     for filename in files:
         try:
-            generate_file(filename)
+            generate_file(filename, info)
             print(f'File {filename} was generated')
         
         except FileExistsError:
